0e9c4abc98d04700e6622c4b40c172a9535417de
Huỳnh Thanh Phong <116573523+Phonghuynh2006@users.noreply.github.com>
2026-03-15 16:36:13 +0700

    Update messages and improve user prompts in bot.js

--- bot.js
+++ bot.js
@@ -19,37 +19,38 @@ client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
   if (message.content.startsWith("!play")) {
+
     const args = message.content.split(" ");
     const url = args[1];
 
     const voiceChannel = message.member.voice.channel;
 
     if (!voiceChannel) {
-      return message.reply("Bạn phải vào voice channel trước!");
+      return message.reply("Bạn phải vào voice trước!");
     }
 
     if (!url) {
-      return message.reply("Hãy gửi link YouTube!");
+      return message.reply("Bạn phải gửi link nhạc!");
     }
 
     const connection = joinVoiceChannel({
       channelId: voiceChannel.id,
       guildId: message.guild.id,
-      adapterCreator: message.guild.voiceAdapterCreator,
+      adapterCreator: message.guild.voiceAdapterCreator
     });
 
     const stream = await play.stream(url);
 
     const resource = createAudioResource(stream.stream, {
-      inputType: stream.type,
+      inputType: stream.type
     });
 
     const player = createAudioPlayer();
 
-    player.play(resource);
     connection.subscribe(player);
+    player.play(resource);
 
-    message.reply("🎵 Đang phát nhạc!");
+    message.reply("🎵 Đang phát nhạc...");
   }
 });
 
