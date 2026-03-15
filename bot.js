2cf637b4eda56d7311905a2e60203976d373bf87
Huỳnh Thanh Phong <116573523+Phonghuynh2006@users.noreply.github.com>
2026-03-15 16:28:13 +0700

    Implement music playback command in bot.js
    Added music playback functionality with !play command.

--- bot.js
+++ bot.js
@@ -1,11 +1,51 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require("@discordjs/voice");
+const play = require("play-dl");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates
+  ]
 });
 
 client.once("ready", () => {
   console.log("Bot đã online!");
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith("!play")) {
+    const args = message.content.split(" ");
+    const url = args[1];
+
+    if (!url) return message.reply("Gửi link YouTube!");
+
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) return message.reply("Bạn phải vào voice trước!");
+
+    const connection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator
+    });
+
+    const stream = await play.stream(url);
+
+    const resource = createAudioResource(stream.stream, {
+      inputType: stream.type
+    });
+
+    const player = createAudioPlayer();
+    connection.subscribe(player);
+
+    player.play(resource);
+
+    message.reply("Đang phát nhạc 🎵");
+  }
 });
 
 client.login(process.env.TOKEN);
