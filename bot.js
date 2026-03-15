3121e2bd34cbd630e0beaad440f13e8a629a5a43
Huỳnh Thanh Phong <116573523+Phonghuynh2006@users.noreply.github.com>
2026-03-15 17:41:36 +0700

    Refactor YouTube playback functionality using ytdl-core

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const {
   getVoiceConnection
 } = require("@discordjs/voice");
 
-const play = require("play-dl");
+const ytdl = require("ytdl-core");
 
 const client = new Client({
   intents: [
@@ -27,53 +27,57 @@ client.on("messageCreate", async (message) => {
 
   if (message.author.bot) return;
 
-if (message.content.startsWith("!play")) {
+  // PLAY
+  if (message.content.startsWith("!play")) {
 
-  const args = message.content.split(" ");
+    const args = message.content.split(" ");
 
-  if (args.length < 2) {
-    return message.reply("❌ Bạn phải gửi link YouTube");
+    if (args.length < 2) {
+      return message.reply("❌ Bạn phải gửi link YouTube");
+    }
+
+    const url = args[1];
+
+    if (!ytdl.validateURL(url)) {
+      return message.reply("❌ Link YouTube không hợp lệ");
+    }
+
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel) {
+      return message.reply("❌ Bạn phải vào voice trước");
+    }
+
+    try {
+
+      const connection = joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: message.guild.id,
+        adapterCreator: message.guild.voiceAdapterCreator
+      });
+
+      const stream = ytdl(url, {
+        filter: "audioonly",
+        highWaterMark: 1 << 25
+      });
+
+      const resource = createAudioResource(stream);
+
+      player.play(resource);
+      connection.subscribe(player);
+
+      message.reply("🎵 Đang phát nhạc");
+
+    } catch (err) {
+
+      console.log("Lỗi phát nhạc:", err);
+      message.reply("❌ Không phát được link");
+
+    }
+
   }
 
-  const url = args[1];
-
-  console.log("Link nhận được:", url); // debug
-
-  const voiceChannel = message.member.voice.channel;
-
-  if (!voiceChannel) {
-    return message.reply("❌ Bạn phải vào voice trước");
-  }
-
-  try {
-
-    const connection = joinVoiceChannel({
-      channelId: voiceChannel.id,
-      guildId: message.guild.id,
-      adapterCreator: message.guild.voiceAdapterCreator
-    });
-
-    const info = await play.video_info(url);
-    const stream = await play.stream(info.video_details.url);
-    
-    const resource = createAudioResource(stream.stream, {
-      inputType: stream.type
-    });
-
-    player.play(resource);
-    connection.subscribe(player);
-
-    message.reply("🎵 Đang phát nhạc");
-
-  } catch (err) {
-
-    console.log("Lỗi phát nhạc:", err);
-    message.reply("❌ Không phát được link");
-
-  }
-
-}
-
+  // STOP
   if (message.content === "!stop") {
 
     player.stop();
@@ -81,6 +85,7 @@ if (message.content.startsWith("!play")) {
 
   }
 
+  // LEAVE
   if (message.content === "!leave") {
 
     const connection = getVoiceConnection(message.guild.id);
